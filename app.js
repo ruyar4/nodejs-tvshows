fbb325fc59abcd1739f28ba5f173c73376c76f84
Rudyard Fuster <ruyar410@gmail.com>
2022-09-18 18:36:17 -0300

    CRUD tvshows

--- app.js
+++ app.js
@@ -1,21 +1,44 @@
 var express = require("express"),
   app = express(),
   bodyParser = require("body-parser"),
-  methodOverride = require("method-override");
-mongoose = require("mongoose");
+  methodOverride = require("method-override"),
+  mongoose = require("mongoose");
 
+// Connection to Mongo DB
+mongoose.connect("mongodb://localhost/tvshows", function(err, res){
+  if(err) throw err;
+  console.log('Connected to DB')
+});
+
+// Midlewares
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
 
+// Import controllers and models
+var models = require("./models/tvshow")(app, mongoose);
+var TVShowCtrl = require("./controllers/tvshows");
+
+// Routes
 var router = express.Router();
-
-router.get("/", function(req, res){
-  res.send("Hello World");
-});
-
 app.use(router);
 
-app.listen(3000, function(){
-  console.log("Node server running on http://localhost:3000")
-})
+var tvshows = express.Router();
+
+tvshows
+  .route("/tvshows")
+  .get(TVShowCtrl.findAllTVShow)
+  .post(TVShowCtrl.addTVShow);
+
+tvshows
+  .route("/tvshows/:id")
+  .get(TVShowCtrl.findById)
+  .put(TVShowCtrl.updateTVShow)
+  .delete(TVShowCtrl.deleteTVShow);
+
+app.use("/api", tvshows);
+
+// Start server
+app.listen(3000, function() {
+  console.log("Node server running on http://localhost:3000");
+});
